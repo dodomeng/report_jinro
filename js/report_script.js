4d58026c33d46d2b4b5d6b9dd248940d412b6b9a
Mo Jeonghwan <mjhmo4062@gmail.com>
2024-11-25 22:47:49 +0900

    실수

--- js/report_script.js
+++ js/report_script.js
@@ -206,14 +206,14 @@ function clearFields() {
     document.getElementById('reportOutput').textContent = '';
 }
 // 마우스 스크롤 시 이벤트 
-document.addEventListener('wheel', function (event) {
-    if (event.deltaY > 0) {
-        document.getElementById("top").style.position = "fixed";
-        document.getElementById("top").style.top = "0px"; // 제목 폼을 top 0 으로 고정
-        document.getElementById("bottom1").style.position = "fixed";
-        document.getElementById("bottom1").style.bottom = "0px"; // 보고서 생성 폼을 bottom 0으로 고정
-    }
-});
+// document.addEventListener('wheel', function (event) {
+//     if (event.deltaY > 0) {
+//         document.getElementById("top").style.position = "fixed";
+//         document.getElementById("top").style.top = "0px"; // 제목 폼을 top 0 으로 고정
+//         document.getElementById("bottom1").style.position = "fixed";
+//         document.getElementById("bottom1").style.bottom = "0px"; // 보고서 생성 폼을 bottom 0으로 고정
+//     }
+// });
 
 
 // 이벤트 리스너 설정
